39aaad6209ed408e785fc71942c15f8ca12605f2
dapie346 <dawid.pietruszewski@gmail.com>
2023-01-30 23:01:33 +0100

    created route query and base for html for highest rated shows; also moved some html into base file for convenience

--- data/queries.py
+++ data/queries.py
@@ -3,3 +3,16 @@ from data import data_manager
 
 def get_shows():
     return data_manager.execute_select('SELECT id, title FROM shows;')
+
+
+def get_most_rated():
+    return data_manager.execute_select(
+        """
+        SELECT shows.id, shows.title, shows.year, shows.runtime, shows.rating, genres.name
+        FROM shows
+        JOIN show_genres ON shows.id = show_genres.show_id
+        JOIN genres ON show_genres.genre_id = genres.id
+        ORDER BY shows.rating
+        DESC 
+        LIMIT 15;
+        """)
--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from dotenv import load_dotenv
 load_dotenv()
 app = Flask('codecool_series')
 
+
 @app.route('/')
 def index():
     shows = queries.get_shows()
@@ -15,6 +16,12 @@ def index():
 @app.route('/design')
 def design():
     return render_template('design.html')
+
+
+@app.route('/shows/most-rated')
+def most_rated():
+    most_rated = queries.get_most_rated()
+    return render_template('most-rated.html', most_rated=most_rated)
 
 
 def main():
